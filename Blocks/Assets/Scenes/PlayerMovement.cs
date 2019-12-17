2154a63cc960e89628a77a77038d652dd7db5458
athessi10 <53184907+athessi10@users.noreply.github.com>
2019-12-17 12:42:29 +0530

    Initial Commit
    The basics of the game

---
+++ Blocks/Assets/Scenes/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+                  
+    }
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * (Time.deltaTime/2));
+
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0,ForceMode.VelocityChange);
+        }
+
+        if(Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0,ForceMode.VelocityChange);
+        }
+    }
+}
